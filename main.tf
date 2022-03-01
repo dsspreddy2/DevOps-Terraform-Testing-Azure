1bb4bff1e9f08e3506b73cf7e38ec097c4c80268
Siva <dsspreddy2@gmail.com>
2022-03-01 16:39:03 +0530

    updated file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   client_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  client_secret   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_secret   = "xxxxxxx11111111111111111111111111111"
   tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   features {}
 }
