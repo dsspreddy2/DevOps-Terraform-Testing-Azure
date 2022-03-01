69e7fce7f56f1f118246ce397bdf9514b069b762
Siva <dsspreddy2@gmail.com>
2022-03-01 16:22:34 +0530

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "a147bddc-5205-4c9c-bd49-82dd450641be"
+  client_id       = "dfcd937f-9684-4a99-aff6-66db9376167a"
+  client_secret   = "y357Q~SGMuQzff1RfC8PuDcJOk-kuSP_7lFg5"
+  tenant_id       = "d642d5a4-23d1-43fd-85d0-db1bcc06b782"
+  features {}
+}
+
