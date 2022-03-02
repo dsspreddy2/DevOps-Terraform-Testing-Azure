795123129eeacd9b88a82b97177b6f8dd2640270
Siva <dsspreddy2@gmail.com>
2022-03-02 19:37:36 +0530

    added nsg to UAT

---
+++ nsg.tf
@@ -0,0 +1,22 @@
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = "${data.azurerm_resource_group.default.name}"
+
+    security_rule {
+        name                       = "Allow_All"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
